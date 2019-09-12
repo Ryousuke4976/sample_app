45e909cc6a4ead5debd8371c0a7e1bf21dba2dc3
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-09-12 16:53:53 +0900

    [Finish] rails_chapter-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5d4cc7ad11537d1fa4b44464bdf542c973e60ab47c25cfcc32f8ddd159aff8ccdfb2d9f8f3e836dfd65251f7e0773cab357d1bb991c430a222088a9318a4b566'
